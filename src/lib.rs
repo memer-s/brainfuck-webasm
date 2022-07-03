fcbef6faea5193cec18dd352b0c700ee4fa2c8f7
F4NZ41 <mimeremim@gmail.com>
2022-07-04 01:05:25 +0200

    Web Assembly is so cool.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,65 @@
 use wasm_bindgen::prelude::*;
 
 pub fn log(s: &String) {
-    web_sys::console::log_1(&s[..].into())
+   web_sys::console::log_1(&s[..].into())
 }
 
 #[wasm_bindgen]
 struct BF {
-    code: String,
-    program_counter: u8,
-    memory: [u8; 30]
+   code: Vec<u8>,
+   code_len: u8,
+   program_counter: u8,
+   memory: [u8; 256],
+   memory_counter: u8,
 }
 
 #[wasm_bindgen]
 impl BF {
-    pub fn new() -> BF {
-        let code = String::from("");
-        BF {
-            code,
-            program_counter: 0,
-            memory: [0; 30]
-        }
-    }
+   pub fn new() -> BF {
+      BF {
+         code: vec![],
+         code_len: 0,
+         program_counter: 0,
+         memory: [0; 256],
+         memory_counter: 0
+      }
+   }
 
-    pub fn set_code(&mut self, c: String) {
-        self.code = c.to_string();
-        log(&format!("Setting code to: {}", self.code))
-    }
+   pub fn set_code(&mut self, c: String) {
+      log(&format!("Writing code: \n{}.\n\nto program memory.", &c.replace(" ", "")));
+      self.code = c.into_bytes();
+      self.code_len = self.code.len() as u8;
+   }
 
-    pub fn print(&self) {
-        log(&format!("PC: {}, Memory: {:?}", self.program_counter, self.memory));
-    }
+   pub fn print(&self) {
+      log(&format!("PC: {}/{}, Memory: \n{:?}", self.program_counter, self.code_len, self.memory));
+   }
+
+   pub fn step(&mut self) {
+      if self.code_len == self.program_counter {
+         log(&String::from("EOF!"))
+      } else {
+         match self.code[self.program_counter as usize] {
+            b'+' => {
+               self.memory[self.memory_counter as usize]+=1;
+               self.program_counter+=1;
+            },
+            b'-' => {
+               self.memory[self.memory_counter as usize]-=1;
+               self.program_counter+=1;
+            },
+            b'>' => {
+               self.memory_counter+=1;
+               self.program_counter+=1;
+            }
+            b'<' => {
+               self.memory_counter-=1;
+               self.program_counter+=1;
+            }
+            _ => {
+               log(&format!("Could not Interpret character at index: {}", self.memory_counter))
+            }
+         };
+      }
+   }
 }
