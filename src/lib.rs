d10f0109779fcf7cff8ae441ab727100e30d2b4a
F4NZ41 <mimeremim@gmail.com>
2022-07-05 03:00:23 +0200

    TODO: fix loops

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,8 @@ struct BF {
    program_counter: u8,
    memory: [u8; 256],
    memory_counter: u8,
+   loop_indices: Vec<u8>,
+   loop_depth: u8
 }
 
 #[wasm_bindgen]
@@ -21,17 +23,23 @@ impl BF {
          code_len: 0,
          program_counter: 0,
          memory: [0; 256],
-         memory_counter: 0
+         memory_counter: 0,
+         loop_indices: vec![],
+         loop_depth: 0
       }
    }
     
    pub fn set_code(&mut self, mut c: String) {
+      log(&format!("Writing code: \n{}.\n\nto program memory", &c));
       c = c.replace(" ", "");
       c = c.replace("\n", "");
       c = c.replace("\t", "");
-      log(&format!("Writing code: \n{}.\n\nto program memory.", &c));
       self.code = c.into_bytes();
       self.code_len = self.code.len() as u8;
+      self.program_counter = 0;
+      self.memory = [0; 256];
+      self.memory_counter = 0;
+      self.loop_depth = 0;
    }
 
    pub fn print(&self) {
@@ -39,10 +47,11 @@ impl BF {
    }
 
    pub fn get_state(&self) -> String {
-      format!("{{\"program_counter\": {}, \"memory\": {:?}, \"memory_counter\": {}}}", 
+      format!("{{\"program_counter\": {},\n \"memory\": {:?},\n \"memory_counter\": {},\n \"depth\": {}}}", 
          self.program_counter, 
          self.memory,
          self.memory_counter,
+         self.loop_depth
       )
    }
 
@@ -67,8 +76,48 @@ impl BF {
                self.memory_counter-=1;
                self.program_counter+=1;
             }
+            b'[' => {
+               let mut i = 0;
+               self.loop_depth = 0;
+               loop {
+                  if self.memory[(self.program_counter-i) as usize] == b'[' {
+                     self.loop_depth += 1
+                  }
+                  i+=1;
+                  if self.program_counter-i==0 {
+                     break;
+                  }
+               }
+               if self.memory[self.memory_counter as usize] == 0 {
+                  let mut i = 0;
+                  loop {
+                     if self.memory[i] == b']' {
+                        self.program_counter = (i-1) as u8;
+                        self.loop_depth-=1;
+                        break;
+                     }
+                     i+=1;
+                  }
+               }
+               self.program_counter+=1;
+            },
+            b']' => {
+               let mut i = 0;
+               let mut n = self.loop_depth;
+               loop {
+                  if self.code[(self.program_counter-i) as usize] == b'[' {
+                     if n == 1 {
+                        self.program_counter = self.program_counter-(i+1);
+                        break;
+                     }
+                     n-=1;
+                  }
+                  i+=1;
+               }
+               self.program_counter+=1;
+            }
             _ => {
-               log(&format!("Could not Interpret character at index: {}", self.memory_counter))
+               log(&format!("Could not Interpret character at index: {}", self.memory_counter));
             }
          };
       }
