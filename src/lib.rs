b508f08f80aa177396de702f6d0e910b04ee2147
F4NZ41 <mimeremim@gmail.com>
2022-07-07 22:13:27 +0200

    Added a little documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+// Author: github.com/memer-s.
+// Check out the wikipedia article on brainfuck if you are interested.
+
 use wasm_bindgen::prelude::*;
 extern crate console_error_panic_hook;
 use std::panic;
 
+
+// Helper functions.
 pub fn log(s: &String) {
    web_sys::console::log_1(&s[..].into())
 }
@@ -10,10 +15,17 @@ pub fn err(s: &String) {
    web_sys::console::error_1(&s[..].into())
 }
 
+// -------------------------------
+
+// This function runs after initialization. 
+// It reroutes the Rust errors to the Javascript console
 #[wasm_bindgen]
 pub fn run() {
-    panic::set_hook(Box::new(console_error_panic_hook::hook))
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
 }
+
+
+// -------------------------------
 
 #[wasm_bindgen]
 struct BF {
@@ -35,12 +47,13 @@ impl BF {
          stack: vec![],
       }
    }
-    
+   
+   // This function is called with the code passed as a string,
+   // it then parses the string into a Vector<u8>.
+   // The function also sets the program_counter and the 
+   // memory_counter to 0, and it initializes the memory: [u8; 256].
    pub fn set_code(&mut self, mut c: String) {
       self.code = vec![];
-      c = c.replace(" ", "");
-      c = c.replace("\n", "");
-      c = c.replace("\t", "");
       c.into_bytes().iter().for_each(|char| {
          match char {
              b'+' => self.code.push(*char),
@@ -59,10 +72,14 @@ impl BF {
       self.memory_counter = 0;
    }
 
+   // This function prints the internal state 
+   // of the interpreter to the JS console
    pub fn print(&self) {
       log(&format!("PC: {}/{}, Memory: \n{:?}", self.program_counter, self.code.len(), self.memory));
    }
 
+   // This function gets the internal state of the interpreter
+   // as a stringified JSON object.
    pub fn get_state(&self) -> String {
       format!("{{\n \"program_counter\": {},\n \"memory\": {:?},\n \"memory_counter\": {},\n \"stack\": {:?}\n}}", 
          self.program_counter, 
@@ -71,7 +88,9 @@ impl BF {
          self.stack
       )
    }
-
+   
+   // This runs the code at the program_counter. And increments the program_counter.
+   // The "," operation is not yet implemented.
    pub fn step(&mut self) {
       if self.code.len() == self.program_counter as usize {
          log(&String::from("EOF!"))
